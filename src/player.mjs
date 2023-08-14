d3887d15fb7fc50c07b846e232bc73f8e9d8ebf5
Leandro Cruz <LeandroCruz717@outlook.com>
2023-08-13 22:17:45 -0300

    add IABotPlayer and rename BotPlayer to RandomBotPlayer

--- src/player.mjs
+++ src/player.mjs
@@ -1,7 +1,8 @@
-import promptSync from "prompt-sync";
-import { Board } from "./board.mjs";
-import { sleep } from "./utils.mjs";
- 
+import promptSync from 'prompt-sync';
+import brain from 'brain.js';
+import { Board } from './board.mjs';
+import { sleep } from './utils.mjs';
+
 const prompt = promptSync({ sigint: true });
 
 export class Player {
@@ -9,12 +10,12 @@ export class Player {
     #symbol;
 
     /**
-     * 
-     * @param {string} symbol 
+     *
+     * @param {string} symbol
      */
     constructor(symbol) {
         if (this.constructor === Player) {
-            throw new Error("Can't instantiate abstract class!")
+            throw new Error("Can't instantiate abstract class!");
         }
 
         this.#symbol = symbol;
@@ -25,8 +26,8 @@ export class Player {
     }
 
     /**
-     * 
-     * @param {Board} board 
+     *
+     * @param {Board} board
      * @returns {Promise<number|null>}
      */
     async getMove(board) {
@@ -36,46 +37,46 @@ export class Player {
 
 export class HumanPlayer extends Player {
     /**
-     * 
-     * @param {string} symbol 
+     *
+     * @param {string} symbol
      */
     constructor(symbol) {
         super(symbol);
     }
 
     /**
-     * 
-     * @param {Board} board 
+     *
+     * @param {Board} board
      * @returns {Promise<number>}
      */
     async getMove(board) {
         const emptyCells = board.getEmptyCells();
-        
+
         while (true) {
             const move = parseInt(prompt(`Your turn (Player ${this.symbol}): `));
-            
+
             if (!isNaN(move) && emptyCells.includes(move)) {
                 console.log('\n');
                 return move;
             }
-        
+
             console.log('Invalid move. Try again.');
         }
     }
 }
 
-export class BotPlayer extends Player {
+export class RandomBotPlayer extends Player {
     /**
-     * 
-     * @param {string} symbol 
+     *
+     * @param {string} symbol
      */
     constructor(symbol) {
         super(symbol);
     }
 
     /**
-     * 
-     * @param {Board} board 
+     *
+     * @param {Board} board
      * @returns {Promise<number>}
      */
     async getMove(board) {
@@ -85,5 +86,31 @@ export class BotPlayer extends Player {
         await sleep(Math.random() * (2000 - 500) + 500);
 
         return board.getEmptyCells()[choice];
+    }
+}
+
+export class IABotPlayer extends Player {
+    /**
+     *
+     * @param {string} symbol
+     * @param {brain.NeuralNetwork} neuralNetwork
+     */
+    constructor(symbol, neuralNetwork) {
+        super(symbol);
+        this.neuralNetwork = neuralNetwork;
+    }
+
+    /**
+     *
+     * @param {Board} board
+     * @returns {Promise<number>}
+     */
+    async getMove(board) {
+        const emptyCells = board.getEmptyCells();
+        const currentState = board.getPlacesNet();
+        const actionValues = this.neuralNetwork.run(currentState);
+        const bestActionIndex = actionValues.indexOf(Math.max(...actionValues));
+
+        return emptyCells[bestActionIndex];
     }
 }
